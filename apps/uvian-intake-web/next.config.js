32049cb9a1003036977bb2c72b838aa5a5f124cf
summit-dev-ops <summit.dev.ops@proton.me>
2026-03-31 17:59:38 +0100

    Web update

--- apps/uvian-intake-web/next.config.js
+++ apps/uvian-intake-web/next.config.js
@@ -1,6 +1,5 @@
 //@ts-check
 
- 
 const { composePlugins, withNx } = require('@nx/next');
 
 /**
@@ -9,7 +8,11 @@ const { composePlugins, withNx } = require('@nx/next');
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
-  nx: {},
+  nx: {
+  },
+  experimental: {
+    cpus: 2, // <--- Add this line to limit the workers
+  },
 };
 
 const plugins = [
